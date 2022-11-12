12d2723d8f3a3e336c508877b05855ab478db4ba
Luka Agic <lukagic@gmail.com>
2022-11-12 16:56:10 +0100

    Hello World

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+
+
+<h1>
+    <?php
+        $greeting = "Hello";
+
+        echo $greeting . ' ' .  'World';
+    ?>
+
+</h1>
+
+<?php 
+
+echo '<h2>Hello World</h2>';
+
+?>
+
+
+</body>
+</html>
