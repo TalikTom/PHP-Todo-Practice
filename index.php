b2b73c1ccede935089667963907f3dcadd3de7ff
Luka Agic <lukagic@gmail.com>
2022-11-17 20:38:20 +0100

    Fix the key within $routes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
     '/' => 'controllers/index.php',
-    'about' => 'controllers/about.php',
+    '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php'
 ];
 
